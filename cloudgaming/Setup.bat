4b48b9a2e1c1f2157189cec2f8280f222b9e74bc
Calculator <hjass2865@gmail.com>
2026-07-16 11:27:20 +0800

    Update Setup.bat

--- cloudgaming/Setup.bat
+++ cloudgaming/Setup.bat
@@ -1,1 +1,11 @@
-npm run dist
+@echo off
+cd /d "%~dp0"
+echo 🧹 Cleaning out old module files...
+if exist node_modules rmdir /s /q node_modules
+if exist package-lock.json del /f /q package-lock.json
+echo 📦 Fetching clean Windows packages...
+cmd /c npm install
+echo --------------------------------------
+echo ✅ Windows Setup Complete!
+echo --------------------------------------
+pause
